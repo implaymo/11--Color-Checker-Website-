224b9fe90354bddb8b70f7bd3383e10d064fb473
implaymo <136201112+implaymo@users.noreply.github.com>
2024-04-16 12:14:03 +0100

    Success creating database and table.

--- webpage.py
+++ webpage.py
@@ -2,6 +2,16 @@ from flask import Flask, flash, redirect, request, url_for, render_template
 import os
 from werkzeug.utils import secure_filename 
 from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+
 
 load_dotenv()
 UPLOAD_FODLER = 'static\images'
@@ -12,6 +22,18 @@ SECRET_KEY = os.getenv("secret_key")
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FODLER
 app.config["SECRET_KEY"] = SECRET_KEY
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+db.init_app(app)
+
+class ImageStore(db.Model):
+    __tablename__ = 'image_store'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    img_url: Mapped[str] = mapped_column(unique=True)
+ 
+
+with app.app_context():
+    db.create_all()
+
 
 def allowed_file(filename):
     return '.' in filename and \
